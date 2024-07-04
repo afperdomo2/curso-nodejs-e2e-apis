b31085bd0952dd0964643da4faaf7d87b839063e
afperdomo2 <alvarado0925@gmail.com>
2024-07-04 18:00:30 -0500

    Implementa buenas prácticas en los tests

--- __tests__/e2e/app.test.js
+++ __tests__/e2e/app.test.js
@@ -1,22 +1,26 @@
 const request = require('supertest');
 const express = require('express');
 
-// Crea una instancia de express
-const app = express();
-
-// Crea una ruta que responde con un JSON
-app.get('/hello', (req, res) => {
-  res.status(200).json({ name: 'mono' });
-});
-
-// Inicia el servidor en el puerto 9020
-app.listen(9020);
-
-// Crea una instancia de supertest pasando la app de express
-const api = request(app);
-
 // Grupo de tests para la app
 describe('tests for app', () => {
+  let app = null;
+  let server = null;
+  let api = null;
+
+  // Antes de cada test
+  beforeEach(() => {
+    // Crea una instancia de express
+    app = express();
+    // Crea una ruta que responde con un JSON
+    app.get('/hello', (req, res) => {
+      res.status(200).json({ name: 'mono' });
+    });
+    // Inicia el servidor en el puerto 9020
+    server = app.listen(9020);
+    // Crea una instancia de supertest pasando la app de express
+    api = request(app);
+  });
+
   // Realiza una petición GET a la ruta /hello
   test('GET /hello', async () => {
     const response = await api.get('/hello');
@@ -34,5 +38,9 @@ describe('tests for app', () => {
     expect(response.headers['content-type']).toEqual(
       'application/json; charset=utf-8'
     );
+  });
+
+  afterEach(() => {
+    server.close();
   });
 });
