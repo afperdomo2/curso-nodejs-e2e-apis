70f780b0799c4eeacba115d4fadb8abe88249759
afperdomo2 <alvarado0925@gmail.com>
2024-07-04 17:45:04 -0500

    Crea la primera prueba E2E

---
+++ __tests__/e2e/app.test.js
@@ -0,0 +1,38 @@
+const request = require('supertest');
+const express = require('express');
+
+// Crea una instancia de express
+const app = express();
+
+// Crea una ruta que responde con un JSON
+app.get('/hello', (req, res) => {
+  res.status(200).json({ name: 'mono' });
+});
+
+// Inicia el servidor en el puerto 9020
+app.listen(9020);
+
+// Crea una instancia de supertest pasando la app de express
+const api = request(app);
+
+// Grupo de tests para la app
+describe('tests for app', () => {
+  // Realiza una petición GET a la ruta /hello
+  test('GET /hello', async () => {
+    const response = await api.get('/hello');
+
+    // Verifica que la respuesta sea verdadera
+    expect(response).toBeTruthy();
+
+    // Verifica que el status code sea 200
+    expect(response.statusCode).toEqual(200);
+
+    // Verifica que el body sea igual a { name: 'mono' }
+    expect(response.body.name).toEqual('mono');
+
+    // Verifica que el header content-type sea igual a 'application/json; charset=utf-8'
+    expect(response.headers['content-type']).toEqual(
+      'application/json; charset=utf-8'
+    );
+  });
+});
