4a7bcf10327bd20c5f33e0000c67bc28c61bc467
afperdomo2 <alvarado0925@gmail.com>
2024-07-18 10:22:27 -0500

    Crea un test para endpoints con accessToken

---
+++ __tests__/e2e/profile.test.js
@@ -0,0 +1,49 @@
+const request = require('supertest');
+
+const createApp = require('../../src/app');
+const { models } = require('../../src/db/sequelize');
+
+// Grupo de tests para la app
+describe('tests for /profile path', () => {
+  let app = null;
+  let server = null;
+  let api = null;
+
+  // Se ejecuta antes de todos los tests
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(9021);
+    api = request(app);
+  });
+
+  describe('GET /profile/my-user', () => {
+    const MY_USER_PATH = '/api/v1/profile/my-user';
+    let accessToken = null;
+    let userData = null;
+
+    beforeAll(async () => {
+      const firstUser = await models.User.findOne();
+      const inputData = { email: firstUser.email, password: 'admin123' };
+      const { body } = await api.post('/api/v1/auth/login').send(inputData);
+      accessToken = body.access_token;
+      userData = body.user;
+    });
+
+    test('Should return a user', async () => {
+      const auth = { Authorization: `Bearer ${accessToken}` };
+      const { statusCode, body } = await api.get(MY_USER_PATH).set(auth);
+      expect(statusCode).toEqual(200);
+      expect(body.email).toEqual(userData.email);
+    });
+
+    afterAll(() => {
+      accessToken = null;
+      userData = null;
+    });
+  });
+
+  // Se ejecuta después de todos los tests
+  afterAll(() => {
+    server.close();
+  });
+});
