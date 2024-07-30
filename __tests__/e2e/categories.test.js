699c59dbf27ff38e0e639d70f04a771851ae8462
Andrés Felipe Perdomo <alvarado0925@gmail.com>
2024-07-30 17:37:07 -0500

    Crea un test para validar la creación de una nueva categoría

--- __tests__/e2e/categories.test.js
+++ __tests__/e2e/categories.test.js
@@ -16,8 +16,16 @@ describe('test for /categories path', () => {
     await upSeed();
   });
 
-  describe('POST /categories', () => {
+  describe('POST /categories with admin user', () => {
     const USERS_PATH = '/api/v1/categories';
+    let accessToken = null;
+
+    beforeAll(async () => {
+      const firstUser = await models.User.findOne();
+      const inputData = { email: firstUser.email, password: 'admin123' };
+      const { body } = await api.post('/api/v1/auth/login').send(inputData);
+      accessToken = body.access_token;
+    });
 
     test('Should return 401', async () => {
       const inputData = {
@@ -26,6 +34,27 @@ describe('test for /categories path', () => {
       };
       const { statusCode } = await api.post(USERS_PATH).send(inputData);
       expect(statusCode).toEqual(401);
+    });
+
+    test('Should return a new category', async () => {
+      const inputData = {
+        name: 'Categoría nueva',
+        image: 'https://api.lorem.space/image/game?w=150&h=220',
+      };
+      const auth = { Authorization: `Bearer ${accessToken}` };
+      const { statusCode, body } = await api
+        .post(USERS_PATH)
+        .send(inputData)
+        .set(auth);
+      expect(statusCode).toEqual(201);
+      // Validar con la base de datos
+      const category = await models.Category.findByPk(body.id);
+      expect(category.name).toEqual(inputData.name);
+      expect(category.image).toEqual(inputData.image);
+    });
+
+    afterAll(() => {
+      accessToken = null;
     });
   });
 
