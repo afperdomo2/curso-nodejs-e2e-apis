c229f900e4a8bd5264fb143ed4f35d2d65220064
Andrés Felipe Perdomo <alvarado0925@gmail.com>
2024-07-30 17:42:45 -0500

    Reorganiza los tests de categories

--- __tests__/e2e/categories.test.js
+++ __tests__/e2e/categories.test.js
@@ -5,9 +5,14 @@ const { models } = require('../../src/db/sequelize');
 const { upSeed, downSeed } = require('./utils/umzug');
 
 describe('test for /categories path', () => {
+  const USERS_PATH = '/api/v1/categories';
+  const IMAGE_URL = 'https://api.lorem.space/image/game?w=150&h=220';
+
   let app = null;
   let server = null;
   let api = null;
+
+  let accessToken = null;
 
   beforeAll(async () => {
     app = createApp();
@@ -16,10 +21,17 @@ describe('test for /categories path', () => {
     await upSeed();
   });
 
-  describe('POST /categories with admin user', () => {
-    const USERS_PATH = '/api/v1/categories';
-    let accessToken = null;
+  // 1. Grupos de tests sin autenticación
+  describe('POST /categories without auth', () => {
+    test('Should return 401', async () => {
+      const inputData = { name: 'Categoría nueva', image: IMAGE_URL };
+      const { statusCode } = await api.post(USERS_PATH).send(inputData);
+      expect(statusCode).toEqual(401);
+    });
+  });
 
+  // 2. Grupos de tests con autenticación de usuario admin
+  describe('POST /categories with admin user', () => {
     beforeAll(async () => {
       const firstUser = await models.User.findOne();
       const inputData = { email: firstUser.email, password: 'admin123' };
@@ -27,20 +39,8 @@ describe('test for /categories path', () => {
       accessToken = body.access_token;
     });
 
-    test('Should return 401', async () => {
-      const inputData = {
-        name: 'Categoría nueva',
-        image: 'https://api.lorem.space/image/game?w=150&h=220',
-      };
-      const { statusCode } = await api.post(USERS_PATH).send(inputData);
-      expect(statusCode).toEqual(401);
-    });
-
     test('Should return a new category', async () => {
-      const inputData = {
-        name: 'Categoría nueva',
-        image: 'https://api.lorem.space/image/game?w=150&h=220',
-      };
+      const inputData = { name: 'Categoría nueva', image: IMAGE_URL };
       const auth = { Authorization: `Bearer ${accessToken}` };
       const { statusCode, body } = await api
         .post(USERS_PATH)
